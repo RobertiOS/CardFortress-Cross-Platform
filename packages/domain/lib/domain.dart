3b9a5b578a46ff8c9aa361817d6c6d11464c4492
Roberto Corrales <robert.ios.developer@gmail.com>
2024-03-20 14:58:47 -0600

    added dummy implementation for credit cards repository

--- packages/domain/lib/domain.dart
+++ packages/domain/lib/domain.dart
@@ -3,3 +3,5 @@ library domain;
 export './src/interfaces/authentication_repository.dart';
 export './src/use_cases/authentication_usecase.dart';
 export './src/di/configure_di.dart';
+export './src/entities/creditcard.dart';
+export './src/interfaces/creditcards_list_repository.dart';
